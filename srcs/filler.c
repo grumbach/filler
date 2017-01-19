a0054e59201039f6b68cce40fcdcfad779494ef6
Anselme GRUMBACH <agrumbac@e2r12p12.42.fr>
2017-01-19 16:47:57 +0100

    RANDFILL WITHOUT COMMENTS

--- srcs/filler.c
+++ srcs/filler.c
@@ -6,7 +6,7 @@
 /*   By: agrumbac <agrumbac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/11 03:18:10 by agrumbac          #+#    #+#             */
-/*   Updated: 2017/01/19 16:44:01 by agrumbac         ###   ########.fr       */
+/*   Updated: 2017/01/19 16:46:25 by agrumbac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ static char		**map_read(int y, int x, int location, int *yx)
 	char		*line;
 	int			i;
 
-							ft_printf("y=%d x=%d\n", y, x);
+//							ft_printf("y=%d x=%d\n", y, x);
 	i = 0;
 	yx[0] = y;
 	yx[1] = x;
@@ -55,7 +55,7 @@ static char		**map_read(int y, int x, int location, int *yx)
 			return (0);
 		if (!(map[i] = ft_strdup(line + location)))
 			return (0);
-							ft_printf("%.3d %s\n", i, map[i]);
+//							ft_printf("%.3d %s\n", i, map[i]);
 		i++;
 		ft_memdel((void**)&line);
 	}
@@ -66,14 +66,14 @@ static int		filler(t_fill *fill, t_xy *ret, char *line)
 {
 	int		gnl;
 
-							ft_printf("%42	start parse\n");
+//							ft_printf("%42	start parse\n");
 	gnl = 1;
 	if (!line)
 		gnl = ft_get_next_line(0, &line);
 	while (gnl)
 	{
-							ft_printf("%42	reading...\n");
-							ft_printf("%42	line{%s}\n", line);
+//							ft_printf("%42	reading...\n");
+//							ft_printf("%42	line{%s}\n", line);
 		if (line[0] == 0)
 			return (0);
 		else if (line[0] == 'P' && !(fill->map))
@@ -93,7 +93,7 @@ static int		filler(t_fill *fill, t_xy *ret, char *line)
 		gnl = ft_get_next_line(0, &line);
 	}
 	ft_memdel((void**)&line);
-							ft_printf("%42	end parse\n");
+//							ft_printf("%42	end parse\n");
 	return (1);
 }
 
@@ -104,7 +104,7 @@ int				main(void)
 	t_xy		ret;
 	int			gnl;
 
-							ft_printf("%42	called me\n");
+//							ft_printf("%42	called me\n");
 	ft_bzero(&fill, sizeof(t_fill));
 	gnl = ft_get_next_line(0, &line);
 	while (gnl != -1)
@@ -117,17 +117,17 @@ int				main(void)
 				if (!(filler(&fill, &ret, NULL)))
 					return (0);
 				ft_memdel((void**)&line);
-							ft_printf("%42	player : %c\n", fill.player);
+//							ft_printf("%42	player : %c\n", fill.player);
 			}
 			else if (!(filler(&fill, &ret, line)))
 				return (0);
-						ft_printf("%42	returned(%d %d)\n", ret.y, ret.x);
+//						ft_printf("%42	returned(%d %d)\n", ret.y, ret.x);
 			ft_putstr(ft_itoa(ret.y));
 			ft_putstr(" ");
 			ft_putendl(ft_itoa(ret.x));
 		}
 		gnl = ft_get_next_line(0, &line);
 	}
-							ft_printf("%42	bye\n");
+//							ft_printf("%42	bye\n");
 	return (0);
 }
